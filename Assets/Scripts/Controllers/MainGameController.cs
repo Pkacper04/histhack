7c7bf009d2b893bb5ec7d52fe16033e4eba9a972
Kacper Potaczała <Kacperpotaczala001@gmail.com>
2023-04-06 20:26:27 +0200

    Finished save system

--- Assets/Scripts/Controllers/MainGameController.cs
+++ Assets/Scripts/Controllers/MainGameController.cs
@@ -1,25 +1,43 @@
+using Histhack.Core.Events;
+using Histhack.Core.SaveLoadSystem;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Histhack.Core
 {
     public class MainGameController : Singleton<MainGameController>
     {
 
-        #region SerializedProperties
+        #region SerializedVariables
 
         [SerializeField]
         private AddictionalMethods addictionalMethods;
 
-        #endregion SerializedProperties
+        #endregion SerializedVariables
+
+        #region PrivateVariables
+
+        private GameEvents gameEvents;
+        private PlayerEvents playerEvents;
+
+        private DataManager dataManager;
+
+        #endregion PrivateVariables
 
 
         #region PublicProperties
 
         public AddictionalMethods AddictionalMethods { get => addictionalMethods; }
 
+        public GameEvents GameEvents { get => gameEvents; }
+        public PlayerEvents PlayerEvents { get => playerEvents; }
+
+        public DataManager DataManager { get => dataManager; }
         #endregion PublicProperties
+
 
 
 
@@ -33,6 +51,32 @@ namespace Histhack.Core
             {
                 DontDestroyOnLoad(this);
             }
+
+            InitializeControllers();
+        }
+
+        private void OnEnable()
+        {
+            SceneManager.activeSceneChanged += LoadScene;
+        }
+
+        private void OnDisable()
+        {
+            SceneManager.activeSceneChanged -= LoadScene;
+        }
+
+        private void LoadScene(Scene arg0, Scene arg1)
+        {
+            dataManager.LoadGame();
+        }
+
+        private void InitializeControllers()
+        {
+            gameEvents = new GameEvents();
+            playerEvents = new PlayerEvents();
+
+            dataManager = new DataManager();
+            dataManager.Init();
         }
     }
 }
