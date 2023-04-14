accea66d548a44b864d19b1f4d5e778eb9f0fd73
Robin Verbist <robinv175@gmail.com>
2023-04-14 11:47:19 +0200

    Added final game intro scene and dialogues prefabs

--- Assets/Scripts/Controllers/GameIntroStartDialogue.cs
+++ Assets/Scripts/Controllers/GameIntroStartDialogue.cs
@@ -1,4 +1,5 @@
 using Histhack.Core;
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,15 +11,16 @@ public class GameIntroStartDialogue : MonoBehaviour
     private Template_UIManager manager;
     [SerializeField]
     private VIDE_Assign VIDE;
+    [SerializeField, Scene]
+    private string mainGameScene;
 
-    
     void Start()
     {
         //MainGameController.Instance.EndTransition(AnimationTypes.CanvasFade, () => manager.Interact(VIDE));
-        manager.Interact(VIDE);
+        MainGameController.Instance.EndTransition(AnimationTypes.AnchoreMovement, () => manager.Interact(VIDE));
     }
-    public void GoToNextScene(int sceneIndex)
+    public void GoToNextScene()
     {
-        MainGameController.Instance.StartTransition(AnimationTypes.CanvasFade, () => SceneManager.LoadScene(sceneIndex));
+        MainGameController.Instance.StartTransition(AnimationTypes.AnchoreMovement, () => SceneManager.LoadScene(mainGameScene));
     }
 }
